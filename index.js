13b2f22cffc2d2b1be4a302423c9cb09e784dc27
Jenny Lin <125613441+jennylin2003@users.noreply.github.com>
2023-09-15 17:56:54 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const setupTextarea = document.getElementById('setup-textarea') 
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('nellie-bot-text')
+
+const apiKey = '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'
+const url = 'https://api.openai.com/v1/completions'
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  if (setupTextarea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    nellieBotText.innerText = `Ok, just wait a second while my digital brain digests that...`
+  }
+  fetchBotReply();
+})
+
+async function fetchBotReply(){
+  try {
+    const response = await openaiClient.createCompletion({
+    'model': 'text-davinci-003',
+    'prompt': 'Say something emotional in 4 words.'
+  })
+  console.log(response)
